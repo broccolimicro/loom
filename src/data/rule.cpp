8b7f40e34e95b577745d3bc88b0c2e195007e9a3
unknown <unknown>
2013-03-11 18:09:15 +0000

    cleaned up the code a little
    git-svn-id: file:///home/nbingham/backup/private-repos/trunk/Software/Work/chp@304 404bc64d-9d1b-498e-bf1b-9a6e5dac48b1

--- src/data/rule.cpp
+++ src/data/rule.cpp
@@ -379,130 +379,15 @@ void rule::clear()
 	down_implicants.clear();
 }
 
-/*//Reduce all implicants to prime
-rule reduce_to_prime(rule pr)
-{
-	rule result = pr;
-	if (result.implicants.size() < 2)
-		return result;
-	//Reduce to prime guards
-	//Totally is a more efficient/logical way to do this
-	size_t i = 0;
-	size_t j = 0;
-	bool removed_junk;
-	removed_junk = true;
-	while(i != result.implicants.size()-1)
-	{
-		removed_junk = false;
-		for (i = 0; (i < result.implicants.size()-1) && !removed_junk; i++)
-		{
-			for (j = i+1; j < result.implicants.size() && !removed_junk; j++)
-			{
-				int unneeded_index;
-				//cout << i << ": " << result.implicants[i] << endl;
-				//cout << j << ": " << result.implicants[j] << endl;
-				unneeded_index = which_index_unneeded(result.implicants[i], result.implicants[j]);
-				//cout << "Between " << i << " and " << j <<" Unneeded = " << unneeded_index << endl;
-
-				if(unneeded_index != -1)
-				{
-					result.implicants[i][unneeded_index].data = "X";
-					result.implicants[j][unneeded_index].data = "X";
-					removed_junk = true;
-				}
-			}//inner for
-		}//Outer for
-	} // while
-	return result;
-}
-
-rule remove_too_strong(rule pr)
-{
-	rule result = pr;
-	if (result.implicants.size() < 2)
-		return result;
-	//Eliminate all 'unneccisarily strong' guards
-	//Totally is a more efficient/logical way to do this
-	size_t i = 0;
-	size_t j = 0;
-	bool removed_junk;
-	removed_junk = true;
-	while(i != result.implicants.size()-1)
-	{
-		removed_junk = false;
-		for (i = 0; (i < result.implicants.size()-1) && !removed_junk; i++)
-		{
-			for (j = i+1; j < result.implicants.size() && !removed_junk; j++)
-			{
-				int weaker_result;
-				//cout << i << ": " << result.implicants[i] << endl;
-				//cout << j << ": " << result.implicants[j] << endl;
-				weaker_result = who_weaker(result.implicants[i], result.implicants[j]);
-				//cout << "Between " << i << " and " << j <<" who_weaker = " << weaker_result << endl;
-				if(weaker_result == -1)
-				{
-					vector<state>::iterator vi = result.implicants.begin();
-					for(size_t counter = 0; counter < i; counter++)
-						vi++;
-					result.implicants.erase(vi);
-					removed_junk = true;
-				}
-				else if(weaker_result == 1)
-				{
-					vector<state>::iterator vi = result.implicants.begin();
-					for(size_t counter = 0; counter < j; counter++)
-						vi++;
-					result.implicants.erase(vi);
-					removed_junk = true;
-				}
-				else if(weaker_result == 2)
-				{
-					vector<state>::iterator vi = result.implicants.begin();
-					for(size_t counter = 0; counter < i; counter++)
-						vi++;
-					result.implicants.erase(vi);
-					removed_junk = true;
-				}
-			}//inner for
-		}//Outer for
-	} // while
-	return result;
-}
-
-//Given a single rule, minimize the implicants to that rule
-rule minimize_rule(rule pr)
-{
-	rule result = pr;
-	result = remove_too_strong(result);
-	result = reduce_to_prime(result);
-	result = remove_too_strong(result);
-	//cout << "finished " << pr.right << endl;
-	return result;
-}
-
-//Given a vector of rules, minimize every implicant in that vector
-vector<rule> minimize_rule_vector(vector<rule> prs)
-{
-	vector<rule> result = prs;
-
-	for (int i = 0; i < (int)result.size(); i++)
-	{
-		//cout << "trying " << prs[i].right << " ("<< i << ")" << endl;
-		result[i] = minimize_rule(result[i]);
-	}
-	return result;
-
-}*/
-
 void print_implicant_tags(vector<state> implicants)
 {
 	int size = implicants.size();
-	for(int i = 0; i < size; i++){
-		if(i != 0)
+	for (int i = 0; i < size; i++)
+	{
+		if (i != 0)
 			cout << ", ";
 		cout << implicants[i].tag;
 	}
-
 }
 
 ostream &operator<<(ostream &os, rule r)
